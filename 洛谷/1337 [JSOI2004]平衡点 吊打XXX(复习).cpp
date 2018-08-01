b608e156e901cda8174ad87af0f3697b6bbb3a1a
memset0 <memset0@outlook.com>
2018-08-01 22:05:57 +0800

    2018.8.1(4)

---
+++ 洛谷/1337 [JSOI2004]平衡点 吊打XXX(复习).cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int read() {
+    int x = 0; bool m = 0; char c = getchar();
+    while (!isdigit(c) && c != '-') c = getchar();
+    if (c == '-') m = 1, c = getchar();
+    while (isdigit(c)) x = x * 10 + c - '0', c = getchar();
+    if (m) return -x; else return x;
+}
+
+const int maxn = 1010;
+const double stat = 2000, endt = 1e-19, dtat = 0.996;
+int n, ax[maxn], ay[maxn], aw[maxn];
+double t, sx, sy, sv, ux, uy, uv, vx, vy, vv, ansx, ansy, ansv;
+
+#define sqr(x) ((x) * (x))
+double calc(double x, double y) {
+	double ret = 0;
+	for (int i = 1; i <= n; i++)
+		ret += sqrt(sqr(x - ax[i]) + sqr(y - ay[i])) * aw[i];
+	return ret;
+}
+
+int main() {
+	srand(20040725);
+	n = read();
+	for (int i = 1; i <= n; i++)
+		ax[i] = read(), ay[i] = read(), aw[i] = read();
+	for (int i = 1; i <= n; i++)
+		sx += ax[i], sy += ay[i];
+	sx /= n, sy /= n, sv = calc(sx, sy);
+	ansv = 1e100;
+	for (int i = 1; i <= 8; i++) {
+		ux = sx, uy = sy, uv = sv, t = stat;
+		while (t > endt) {
+			vx = ux + (rand() * 2 - RAND_MAX) * t;
+			vy = uy + (rand() * 2 - RAND_MAX) * t;
+//			printf("%.3lf %.3lf %.3lf ", ux, uy, uv);
+//			printf("%.3lf %.3lf %.3lf\n", vx, vy, vv);
+			vv = calc(vx, vy);
+			if (vv < uv) {
+				ux = vx, uy = vy, uv = vv;
+			} else if (exp((uv - vv) / t) * RAND_MAX > rand()) {
+				ux = vx, uy = vy, uv = vv;
+			}
+			t *= dtat;
+		}
+		if (uv < ansv) {
+			ansx = ux, ansy = uy, ansv = uv;
+		}
+	}
+	printf("%.3lf %.3lf\n", ansx, ansy);
+	return 0;
+}
--- 洛谷/1903 [国家集训队]数颜色.cpp
+++ 洛谷/1903 [国家集训队]数颜色.cpp
@@ -17,7 +17,7 @@ char readc() {
 
 const int maxn = 50010;
 int n, m, l, r, sqn, sqt, p, t, opt, now, z;
-int a[maxn], cnt[1000010], ans[maxn];
+int a[maxn], tmp, bel[maxn], cnt[1000010], ans[maxn];
 struct Query {
 	int l, r, t, i;
 } q[maxn];
@@ -25,45 +25,37 @@ struct Modify {
 	int k, v;
 } d[maxn];
 bool cmp(Query a, Query b) {
-	if (a.t / sqt != a.t / sqt) return a.t < b.t;
-	if (a.l / sqn != b.l / sqn) return a.l < b.l;
-	return a.r < b.r;
+	if (bel[a.l] != bel[b.l]) return a.l < b.l;
+	if (bel[a.r] != bel[b.r]) return a.r < b.r;
+	return a.t < b.t;
 }
-void ist(int t) {
-//	printf("ist %d\n", t);
-	if (!cnt[t]) now++;
-	cnt[t]++;
-}
-void rmv(int t) {
-//	printf("rmv %d\n", t);
-	cnt[t]--;
-	if (!cnt[t]) now--;
-}
-void upd(int i) {
+inline void upd(int i) {
 //	printf("upd %d %d\n", d[i].k, d[i].v);
-	if (l <= d[i].k && d[i].k <= r) rmv(a[d[i].k]);
+//	tmp++;
+	if (l <= d[i].k && d[i].k <= r) now -= !--cnt[a[d[i].k]];
 	swap(a[d[i].k], d[i].v);	
-	if (l <= d[i].k && d[i].k <= r) ist(a[d[i].k]);
+	if (l <= d[i].k && d[i].k <= r) now += !cnt[a[d[i].k]]++;
 }
 int main() {
-	freopen("INPUT", "r", stdin);
-	freopen("OUTPUT", "w", stdout);
-	n = read(), m = read(), sqn = sqrt(n);
+//	freopen("INPUT", "r", stdin);
+//	freopen("OUTPUT", "w", stdout);
+	n = read(), m = read();
 	for (int i = 1; i <= n; i++)
 		a[i] = read();
 	for (int i = 1; i <= m; i++)
 		if ((opt = readc()) == 'Q') q[++p] = {read(), read(), t, p};
 		else d[++t] = {read(), read()};
-	sqt = sqrt(t);
+	int sqn = ceil(exp((log(n) + log(t)) / 3));
+	for (int i = 1; i <= n; i++)
+		bel[i] = (i - 1) / sqn + 1;
 	sort(q + 1, q + p + 1, cmp);
 	l = 1, r = 0, t = 0;
-	puts("!");
 	for (int i = 1; i <= p; i++) {
-//		printf(">>> %d %d %d %d\n", q[i].l, q[i].r, q[i].t, q[i].i);
-		while (l < q[i].l) rmv(a[l++]);
-		while (l > q[i].l) ist(a[--l]);
-		while (r > q[i].r) rmv(a[r--]);
-		while (r < q[i].r) ist(a[++r]);
+//		printf("%d %d %d %d\n", q[i].l/sqn, q[i].r/sqn, q[i].t, q[i].i);
+		while (l < q[i].l) now -= !--cnt[a[l++]]/*, tmp++*/;
+		while (l > q[i].l) now += !cnt[a[--l]]++/*, tmp++*/;
+		while (r > q[i].r) now -= !--cnt[a[r--]]/*, tmp++*/;
+		while (r < q[i].r) now += !cnt[a[++r]]++/*, tmp++*/;
 		while (t < q[i].t) upd(++t);
 		while (t > q[i].t) upd(t--);
 		ans[q[i].i] = now;
