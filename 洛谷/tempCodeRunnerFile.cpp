ad34f33c494baedf6396cc6242bdd53350161446
memset0 <memset0@outlook.com>
2018-08-05 19:01:44 +0800

    2018.8.5(3)

--- 洛谷/tempCodeRunnerFile.cpp
+++ 洛谷/tempCodeRunnerFile.cpp
@@ -1,2 +1,12 @@
-for (int i = 1; i <= (n << 2); i++)
-        
+
+		if (!flag) {
+			if (!--gap[dep[u]]) break;
+			dep[u] = en;
+			for (int i = hed[u]; ~i; i = nxt[i])
+				if (val[i] && dep[u] > dep[to[i]] + 1) {
+					dep[u] = dep[to[i]] + 1;
+					cur[u] = i;
+				}
+			++gap[dep[u]];
+			if (u != s) u = to[pre[u] ^ 1];
+		}
