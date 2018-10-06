fe3c48f24424dca47f2334dd92319b07a223025c
memset0 <memset0@outlook.com>
2018-10-06 12:41:16 +0800

    20181006

---
+++ CodeForces/Codeforces Round #514 (Div. 2)/A.cpp
@@ -0,0 +1,41 @@
+// ==============================
+//  author: memset0
+//  website: https://memset0.cn
+// ==============================
+#include <bits/stdc++.h>
+#define ll long long
+#define rep(i,l,r) for (int i = l; i <= r; i++)
+#define getc(x) getchar(x)
+#define putc(x) putchar(x)
+
+template <typename T> inline void read(T &x) {
+	x = 0; register char ch; register bool fl = 0;
+	while (ch = getc(), ch < 48 || 57 < ch) fl ^= ch == '-'; x = (ch & 15);
+	while (ch = getc(), 47 < ch && ch < 58) x = (x << 1) + (x << 3) + (ch & 15);
+	if (fl) x = -x;
+}
+template <typename T> inline void readc(T &x) {
+	while (x = getc(), !islower(x) && !isupper(x));
+}
+template <typename T> inline void print(T x, char c = ' ') {
+	static int buf[40];
+	if (x == 0) { putc('0'); putc(c); return; }
+	if (x < 0) putc('-'), x = -x;
+	for (buf[0] = 0; x; x /= 10) buf[++buf[0]] = x % 10 + 48;
+	while (buf[0]) putc((char) buf[buf[0]--]);
+	putc(c);
+}
+ll n, len, x, ans, l, t, r, lastr;
+
+int main() {
+    read(n), read(len), read(x);
+    for (int i = 1; i <= n; i++) {
+        read(l), read(t);
+        r = l + t;
+        ans += (l - lastr) / x;
+        lastr = r;
+    }
+    print(ans + (len - lastr) / x, '\n');
+
+	return 0;
+}
