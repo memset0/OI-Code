858cfe390d00d5e8c84568b617a6b5bb34fc304f
memset0 <memset0@outlook.com>
2018-10-11 14:25:14 +0800

    20181011

--- 洛谷/4169 [Violet]天使玩偶 SJY摆棋子.cpp
+++ 洛谷/4169 [Violet]天使玩偶 SJY摆棋子.cpp
@@ -26,20 +26,25 @@ template <typename T> inline void print(T x, char c = ' ') {
 	putc(c);
 }
 
-const int maxn = 600010;
+const int maxn = 600010, inf = 1e9;
 
-int n, m;
+int n, m, x, y, rt, opt, cnt, top, ans, FG;
+int stk[maxn];
 
 struct point {
 	int x[2];
 	point () {}
 	point (int a, int b) { x[0] = a, x[1] = b; } 
-};
+} p[maxn]; 
+bool operator < (const point &a, const point &b) {
+	return a.x[FG] < b.x[FG];
+}
 
 struct node {
 	int ch[2], min[2], max[2];
+	int cnt;
 	point a;
-};
+} e[maxn];
 
 #define lc e[u].ch[0]
 #define rc e[u].ch[1]
@@ -70,27 +75,49 @@ void pia(int u, int k) {
 int build(int l, int r, int flag) {
 	if (l > r) return 0;
 	int mid = (l + r) >> 1;
-	FG = flag, std::nth_element(p + l. p + r + 1, p + mid);
+	FG = flag, std::nth_element(p + l, p + r + 1, p + mid);
 	int u = newnode(p[mid]);
 	lc = build(l, mid - 1, flag);
 	rc = build(mid + 1, r, flag);
-	return update(u);
+	return update(u), u;
 }
 
 void check(int &u, int flag) {
 	static int ratio = 4;
 	if (e[lc].cnt * ratio < e[rc].cnt || e[rc].cnt * ratio < e[lc].cnt)
-		pia(u, p[u].cnt), u = build(1, e[u].cnt, flag); 
+		pia(u, e[u].cnt), u = build(1, e[u].cnt, flag); 
 }
 
 void insert(int &u, point a, int flag) {
-	if (!u) return (void) u = newnode();
-	insert(a.x[flag] <= e[u].a[flag] ? lc : rc, a, flag ^ 1);
+	if (!u) return (void)(u = newnode(a));
+	insert(a.x[flag] <= e[u].a.x[flag] ? lc : rc, a, flag ^ 1);
 	update(u), check(u, flag);
 }
 
+inline int min_dis(const point &a, int min[2], int max[2]) {
+	return std::min(std::abs(a.x[0] - min[0]), std::abs(a.x[0] - max[0])) +
+		std::min(std::abs(a.x[1] - min[1]), std::abs(a.x[1] - max[1]));
+}
+inline int max_dis(const point &a, int min[2], int max[2]) {
+	return std::max(std::abs(a.x[0] - min[0]), std::abs(a.x[0] - max[0])) +
+		std::max(std::abs(a.x[1] - min[1]), std::abs(a.x[1] - max[1]));
+}
+
+void query(int u, point a) {
+	if (!u) return;
+	ans = std::min(ans, max_dis(a, e[u].min, e[u].max));
+	if (min_dis(a, e[u].min, e[u].max) >= ans) return;
+	query(lc, a), query(rc, a);
+}
+
+void dfs(int u) {
+	if (lc) dfs(lc);
+	printf("%d : %d %d %d %d %d %d\n", u, e[u].a.x[0], e[u].a.x[1], e[u].min[0], e[u].min[1], e[u].max[0], e[u].max[1]);
+	if (rc) dfs(rc);
+}
+
 int main() {
-	
+	freopen("INPUT", "r", stdin);
 	read(n), read(m);
 	for (int i = 1; i <= n; i++) {
 		read(x), read(y);
@@ -98,13 +125,16 @@ int main() {
 	}
 	for (int i = 1; i <= m; i++) {
 		read(opt), read(x), read(y);
+		printf(">>> %d %d %d\n", opt, x, y);
 		if (opt == 1) {
 			insert(rt, point(x, y), 0);
 		} else {
+			ans = inf;
 			query(rt, point(x, y));
+			print(ans, '\n');
 		}
+		dfs(rt);
 	}
-
 	return 0;
 }
 
