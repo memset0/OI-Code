a078a163cc1e8b2899263c82eb5b81bc41ad12d3
memset0 <memset0@outlook.com>
2018-10-22 13:00:50 +0800

    20181022

--- BZOJ/5017 [Snoi2017]炸弹.cpp
+++ BZOJ/5017 [Snoi2017]炸弹.cpp
@@ -28,7 +28,7 @@ template <typename T> inline void print(T x, char c = ' ') {
 
 #define int long long
 
-const int maxn = 1000010;
+const int maxn = 500010;
 
 int n, m, u, v, w, x, l, r, tn, ans, cnt, pos;
 ll b[maxn]; int q[maxn];
@@ -73,6 +73,7 @@ void tarjan(int u) {
 		}
 	}
 	if (dfn[u] == low[u]) {
+		// printf(">> %d\n", u);
 		++cnt;
 		ls[cnt] = n + 1;
 		while (top) {
@@ -87,7 +88,7 @@ void tarjan(int u) {
 }
 
 signed main() {
-	freopen("1.in", "r", stdin);
+	// freopen("1.in", "r", stdin);
 	read(n);
 	for (int i = 1; i <= n; i++) {
 		read(x), read(r);
@@ -97,6 +98,8 @@ signed main() {
 	std::sort(a + 1, a + n + 1);
 	std::sort(b + 1, b + tn + 1);
 	tn = std::unique(b + 1, b + tn + 1) - b - 1;
+	// for (int i = 1; i <= n; i++)
+		// printf(">> %lld %lld %lld\n", a[i].x, a[i].l, a[i].r);
 	for (int i = 1; i <= n; i++) {
 		a[i].l = std::lower_bound(b + 1, b + tn + 1, a[i].x - a[i].r) - b;
 		a[i].r = std::upper_bound(b + 1, b + tn + 1, a[i].x + a[i].r) - b - 1;
@@ -105,6 +108,7 @@ signed main() {
 	// for (int i = 1; i <= n; i++)
 	// 	printf(">> %d %d %d\n", a[i].x, a[i].l, a[i].r);
 	for (int i = 1; i <= n; i++) {
+		while (top && a[stk[top]].r < a[i].x) top--;
 		if (top && a[stk[top]].r >= a[i].x)
 			add_edge1(stk[top], i);
 		while (top && a[stk[top]].r <= a[i].r) top--;
@@ -112,6 +116,7 @@ signed main() {
 	}
 	top = 0;
 	for (int i = n; i >= 1; i--) {
+		while (top && a[stk[top]].l > a[i].x) top--;
 		if (top && a[stk[top]].l <= a[i].x)
 			add_edge1(stk[top], i);
 		while (top && a[stk[top]].l >= a[i].l) top--;
@@ -142,14 +147,15 @@ signed main() {
 			add_edge2(u, v);
 		}
 	}
+	l = 1, r = 0;
 	for (int i = 1; i <= n; i++)
 		if (!ind[i]) {
 			q[++r] = i;
 		}
-	l = 1;
 	while (l <= r) {
 		u = q[l++];
 		for (int i = hed[u], v = to[i]; i; i = nxt[i], v = to[i]) {
+			// printf("%d -> %d\n", u, v);
 			ls[v] = std::min(ls[u], ls[v]);
 			rs[v] = std::max(rs[u], rs[v]);
 			if (!--ind[v])
