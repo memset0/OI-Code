d015bded2cbd5632d8bbc42e359ac73914b894e4
memset0 <memset0@outlook.com>
2018-11-01 23:34:15 +0800

    20181101

--- 洛谷/1084 疫情控制.cpp
+++ 洛谷/1084 疫情控制.cpp
@@ -26,39 +26,108 @@ template <typename T> inline void print(T x, char c = ' ') {
 	putc(c);
 }
 
-const int maxn = 50010;
+#define int long long
 
-int n, m;
+const int maxn = 50010, maxm = 100010;
+const ll inf = 4000000000000000000;
+
+int n, m, u, v, w, cnt;
+int a[maxn], id[maxn], lef[maxn], col[maxn], nod[maxn], mink[maxn], minv[maxn];
+int f[maxn][20];
+ll l, r, min, mid, ans, lst, g[maxn][20];
+bool tag[maxn], used[maxn];
+
+std::vector < ll > vet[maxn];
+std::vector < std::pair < ll, int > > ned, set;
+
+int tot = 2, hed[maxn], to[maxm], val[maxm], nxt[maxm];
 
 void dfs(int u) {
 	for (int i = hed[u], v = to[i]; i; i = nxt[i], v = to[i])
-		if (v ^ f[u][0]) {
-			f[v][0] = u, g[v][0] = val[i];
+		if (v != f[u][0]) {
+			f[v][0] = u;
+			g[v][0] = val[i];
 			dfs(v);
 		}
 }
 
-bool check(int x) {
-	p = q = 0;
+void color(int u, int c) {
+	col[u] = c;
+	for (int i = hed[u], v = to[i]; i; i = nxt[i], v = to[i])
+		if (v != f[u][0])
+			color(v, c);
+}
+
+bool is_full(int u) {
+	if (tag[u]) return true;
+	bool flag = false;
+	for (int i = hed[u], v = to[i]; i; i = nxt[i], v = to[i])
+		if (v != f[u][0]) {
+			flag = true;
+			if (!is_full(v))
+				return false;
+		}
+	return flag;
+}
+
+bool check(ll x) {
+	// printf("\n=== check %d ===\n", x);
+	memset(tag, 0, sizeof(tag));
+	for (int i = 1; i <= n; i++)
+		vet[i].clear();
+	set.clear();
+	ned.clear();
 	for (int i = 1; i <= m; i++) {
-		memset()
 		u = a[i], lst = x;
 		for (int i = 19; i >= 0; i--)
 			if (lst >= g[u][i]) {
 				lst -= g[u][i];
 				u = f[u][i];
 			}
-		if (!u) {
-			vet[jump(a[i])].push_back(lst);
+		if (u == 0) vet[col[a[i]]].push_back(lst);
+		else tag[u] = 1;
+	}
+	for (int i = 1; i <= cnt; i++)
+		if (is_full(nod[i])) {
+			for (std::vector < ll > ::iterator it = vet[i].begin(); it != vet[i].end(); it++)
+				set.push_back(std::make_pair(*it, i));
+		} else {
+			for (std::vector < ll > ::iterator it = vet[i].begin(); it != vet[i].end(); it++)
+				set.push_back(std::make_pair(*it, i));
+			ned.push_back(std::make_pair(lef[i], i));
 		}
+	std::sort(ned.rbegin(), ned.rend());
+	std::sort(set.rbegin(), set.rend());
+	memset(minv, 63, sizeof(minv));
+	memset(used, 0, sizeof(used));
+	for (int i = 0; i < (int)set.size(); i++)
+		if (set[i].first < minv[set[i].second]) {
+			minv[set[i].second] = set[i].first;
+			mink[set[i].second] = i;
+		}
+	// printf("%d %d\n", set.size(), ned.size());
+	// for (int i = 0; i < (int)ned.size(); i++)
+	// 	printf("(%d %d) ", ned[i].first, ned[i].second);
+	// putc('\n');
+	// for (int i = 0; i < (int)set.size(); i++)
+	// 	printf("(%d %d %d) ", set[i].first, set[i].second, minv[set[i].second]);
+	// putc('\n');
+	for (int i = 0, j = 0; i < (int)ned.size(); i++) {
+		// printf(">>> %d %d\n", i, j);
+		if (minv[ned[i].second] != minv[0] && !used[mink[ned[i].second]]) {
+			// printf("flag\n");
+			used[mink[ned[i].second]] = true;
+			continue;
+		}
+		while ((used[j] || set[j].first < ned[i].first) && j < (int)set.size()) j++;
+		if (j == (int)set.size())
+			return false;
+		used[j++] = true;
 	}
-	for (int i = hed[1], v = to[i]; i; i = nxt[i], v = to[i]) {
-		vrt
-	}
+	return true;
 }
 
-int main() {
-	// freopen("1.in", "r", stdin);
+signed main() {
 	read(n);
 	for (int i = 1; i < n; i++) {
 		read(u), read(v), read(w);
@@ -66,17 +135,28 @@ int main() {
 		nxt[tot] = hed[v], to[tot] = u, val[tot] = w, hed[v] = tot++;
 	}
 	read(m);
-	for (int i = 1; i <= m; i++) {
+	for (int i = 1; i <= m; i++)
 		read(a[i]);
-		v[a[i]]++;
-	}
+	// std::sort(a + 1, a + m + 1);
+	// for (int i = 1; i <= m; i++)
+	// 	print(a[i]);
+	// putc('\n');
 	dfs(1);
 	for (int i = 1; i < 20; i++)
 		for (int j = 1; j <= n; j++) {
 			f[j][i] = f[f[j][i - 1]][i - 1];
 			g[j][i] = g[f[j][i - 1]][i - 1] + g[j][i - 1];
 		}
-	l = 0, r = 1e9;
+	cnt = 0;
+	for (int i = hed[1], v = to[i]; i; i = nxt[i], v = to[i]) {
+		color(v, ++cnt);
+		lef[cnt] = val[i];
+		nod[cnt] = v;
+		id[v] = cnt;
+		// printf("%d %d %d\n", cnt, v, val[i]);
+	}
+	// print(check(4), '\n');
+	ans = -1, l = 0, r = 1e9;
 	while (l <= r) {
 		mid = (l + r) >> 1;
 		if (check(mid)) {
@@ -86,6 +166,6 @@ int main() {
 			l = mid + 1;
 		}
 	}
-	print(ans);
+	print(ans, '\n');
 	return 0;
 }
