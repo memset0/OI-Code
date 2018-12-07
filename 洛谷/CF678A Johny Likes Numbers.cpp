77c66f0b090c5f3a6b2fbcc9dd72ac77415c11e1
memset0 <memset0@outlook.com>
2018-12-07 10:22:00 +0800

    20181207

---
+++ 洛谷/CF678A Johny Likes Numbers.cpp
@@ -0,0 +1,38 @@
+// =================================
+//   author: memset0
+//   date: 2018.12.07 10:03:51
+//   website: https://memset0.cn/
+// =================================
+#include <bits/stdc++.h>
+namespace ringo {
+typedef long long ll;
+
+template < class T >
+inline void read(T &x) {
+    x = 0; register char c = getchar(); register bool f = 0;
+    while (!isdigit(c)) f ^= c == '-', c = getchar();
+    while (isdigit(c)) x = x * 10 + c - '0', c = getchar();
+    if (f) x = -x;
+}
+
+template < class T >
+inline void print(T x) {
+    if (x < 0) putchar('-'), x = -x;
+    if (x > 9) print(x / 10);
+    putchar('0' + x % 10);
+}
+
+template < class T >
+inline void print(T x, char c) {
+    print(x), putchar(c);
+}
+
+ll n, k, s;
+
+void main() {
+    read(n), read(k);
+    s = n / k * k + k;
+    print(s, '\n');
+}
+
+} signed main() { return ringo::main(), 0; }
